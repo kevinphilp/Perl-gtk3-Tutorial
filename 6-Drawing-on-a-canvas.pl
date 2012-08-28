067d1c442b6b6b4a0c5391a427cc357305c773bc
Kevin Philp <kevin@cybercolloids.net>
2012-08-28 20:36:32 +0100

    First working Cairo program

--- 6-Drawing-on-a-canvas.pl
+++ 6-Drawing-on-a-canvas.pl
@@ -1,18 +1,39 @@
-#! /usr/bin/perl
-
-use strict;
+#!/usr/bin/env perl
 use warnings;
-use diagnostics;
-use feature ':5.14';
+use strict;
+use Cairo::GObject;
 use Gtk3 '-init';
-use Cairo;
 
 my $window = Gtk3::Window->new('toplevel');
-$window->set_title("My Title");
-$window->set_position("mouse");
-$window->set_default_size(400, 50);
-$window->set_border_width(20);
-$window->signal_connect (delete_event => sub { Gtk3->main_quit });
+$window->signal_connect( 'destroy' => sub { Gtk3->main_quit } );
 
+my $drawable = Gtk3::DrawingArea->new;
+
+$drawable->signal_connect(
+    'draw' => sub {
+        my ( $widget, $context ) = @_;
+
+        my $alloc = $widget->get_allocation;
+
+        $context->move_to( rand( $alloc->{width} ), rand( $alloc->{height} ) );
+        $context->rel_line_to( 30, 30 );
+        $context->rel_line_to( 30, 30 );
+        $context->stroke;
+
+        $context->set_source_rgb(0.69, 0.19, 0);
+ 		$context->arc(10, 10, 50, 45, 90);
+ 		$context->stroke_preserve;
+
+ 		$context->set_source_rgb (1, 0, 0);
+		$context->fill;
+    }
+);
+
+$window->add($drawable);
 $window->show_all;
+
+$drawable->queue_draw;
+
 Gtk3->main;
+
+=cut
