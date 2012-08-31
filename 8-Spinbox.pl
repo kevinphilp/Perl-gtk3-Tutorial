5ee5d11d4c37a2ed885f362e19cbb15a37ed76a4
kevinphilp <kevin@cybercolloids.net>
2012-08-31 17:05:52 +0100

    Added Spinbox program

---
+++ 8-Spinbox.pl
@@ -0,0 +1,51 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+use feature ':5.14';
+use Gtk3 '-init';
+use Glib qw/TRUE FALSE/;
+
+my $window = Gtk3::Window->new('toplevel');
+$window->set_title("Spinbox Example");
+$window->set_position("mouse");
+$window->set_default_size(200, 50);
+$window->set_border_width(5);
+$window->signal_connect (delete_event => sub { Gtk3->main_quit });
+
+my $vbox = Gtk3::Box->new("vertical", 5);
+$window->add($vbox);
+
+my $adjust = Gtk3::Adjustment->new(5, 0, 10, 1, 10, 0);
+my $spin = Gtk3::SpinButton->new($adjust, 1, 1);
+$vbox->pack_start($spin, FALSE, FALSE, 0);
+
+my $hbox = Gtk3::Box->new("horizontal", 5);
+$vbox->pack_start($hbox, FALSE, FALSE, 0);
+
+my $zero_button = Gtk3::Button->new('Zero');
+$hbox->pack_start($zero_button, FALSE, FALSE, 0);
+$zero_button->signal_connect (clicked => \&zero, "zero");
+
+my $check_accuracy = Gtk3::CheckButton->new_with_label('High accuracy');
+$hbox->pack_start($check_accuracy, FALSE, FALSE, 0);
+$check_accuracy->signal_connect (toggled => \&toggle_accuracy, "accuracy");
+
+$window->show_all;
+Gtk3->main;
+
+sub zero {
+	my ($widget, $data) = @_;
+    $spin->set_value(0);
+}
+
+sub toggle_accuracy{
+	my ($widget, $data) = @_;
+    my $value = $widget->get_active;
+    if ( $value == TRUE ) {
+            $spin->set_digits(2);
+    } else {
+            $spin->set_digits(1);
+    }
+}
