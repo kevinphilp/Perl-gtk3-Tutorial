d88714ecbe41c3ac0e308c9c087e11b4ee58e3e6
kevinphilp <kevin@cybercolloids.net>
2012-10-10 15:46:52 +0100

    Cell renderer demo now working with three columns, editable text and spin box

--- 10a-Cell-renderer-text.pl
+++ 10a-Cell-renderer-text.pl
@@ -2,11 +2,9 @@
 
 use strict;
 use warnings;
-use diagnostics;
-use feature ':5.14';
+use feature ':5.10';
 use Gtk3 '-init';
 use Glib qw/TRUE FALSE/;
-use Data::Dumper;
 
 my $window = Gtk3::Window->new('toplevel');
 $window->set_title("Cell Renderer Example");
@@ -15,69 +13,45 @@ $window->set_default_size(250, 100);
 $window->set_border_width(5);
 $window->signal_connect (delete_event => sub { Gtk3->main_quit });
 
-my $data = Gtk3::ListStore->new( 'Glib::Int', 'Glib::String' );
-
-$data->set( $data->append, 0, 1, 1, 'First bit' );
+my $data = Gtk3::ListStore->new( 'Glib::Int', 'Glib::String', 'Glib::String' );
+$data->set( $data->append, 0, 1, 1, 'First bit');
 $data->set( $data->append, 0, 2, 1, 'second' );
-$data->set( $data->append, 0, 3, 1, 'third' );
-
+$data->set( $data->append, 0, 3, 2, 'third' );
 my $tree = Gtk3::TreeView->new($data);
 
 my $spinner = Gtk3::CellRendererSpin->new;
-$spinner->signal_connect( edited => \&on_amount_edited, $data );
 $spinner->set_property( editable => TRUE );
-
 my $adj = Gtk3::Adjustment->new( 0, 0, 100, 1, 10, 0 );
 $spinner->set_property( adjustment => $adj );
+$spinner->signal_connect( edited => \&on_amount_edited, $data );
+my $col0 = Gtk3::TreeViewColumn->new_with_attributes( 'Number', $spinner, text => 0 );
+$tree->append_column($col0);
 
-my $cs = Gtk3::TreeViewColumn->new_with_attributes( 'Amount', $spinner, text => 0 );
-$tree->append_column($cs);
+my $col1 = Gtk3::TreeViewColumn->new_with_attributes( 'Text', Gtk3::CellRendererText->new, text => 1 );
+$tree->append_column($col1);
 
-my $ct = Gtk3::TreeViewColumn->new_with_attributes( 'Numbers', Gtk3::CellRendererText->new, text => 1 );
-$tree->append_column($ct);
+my $ren_text = Gtk3::CellRendererText->new();
+$ren_text->set_property('editable', TRUE);
+$ren_text->signal_connect (edited => \&cell_edited, $data);
+my $col2 = Gtk3::TreeViewColumn->new_with_attributes( 'Ed Text', $ren_text, text => 2 );
+$tree->append_column($col2);
 
 $window->add($tree);
-
 $window->show_all;
 Gtk3->main;
 
 sub on_amount_edited {
-    my ( $widget, $path, $value, $store ) = @_;
-
+    my ( $widget, $path, $value, $model ) = @_;
     my $path_str = Gtk3::TreePath->new($path);
-    my $iter = $store->get_iter($path_str);
+    my $iter = $model->get_iter($path_str);
+    $model->set( $iter, 0, int($value) );
+}
 
-    $store->set( $iter, 0, int($value) );
+sub cell_edited {
+	my ($cell, $path, $value, $model) = @_;
+	my $path_str = Gtk3::TreePath->new($path);
+	my $iter = $model->get_iter($path_str);
+	$model->set($iter, 2, $value);
 }
 
 
-=head
-
-
-
-        treeview = Gtk.TreeView(model=self.liststore)
-
-        renderer_text = Gtk.CellRendererText()
-        column_text = Gtk.TreeViewColumn("Text", renderer_text, text=0)
-        treeview.append_column(column_text)
-
-        renderer_editabletext = Gtk.CellRendererText()
-        renderer_editabletext.set_property("editable", True)
-
-        column_editabletext = Gtk.TreeViewColumn("Editable Text",
-            renderer_editabletext, text=1)
-        treeview.append_column(column_editabletext)
-
-        renderer_editabletext.connect("edited", self.text_edited)
-
-        self.add(treeview)
-
-    def text_edited(self, widget, path, text):
-        self.liststore[path][1] = text
-
-win = CellRendererTextWindow()
-win.connect("delete-event", Gtk.main_quit)
-win.show_all()
-Gtk.main()
-
-=cut
